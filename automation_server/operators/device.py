a77b58612538e140cd19a7004530fc7d9092cd4d
knowalinski <94801188+knowalinski@users.noreply.github.com>
2022-08-08 09:05:52 +0200

    Add files via upload

---
+++ automation_server/operators/device.py
@@ -0,0 +1,47 @@
+import time
+
+
+class Device:
+    def __init__(self, serial_id):
+        self.serial_id = serial_id
+        self.switch = 0
+
+    def override_switch(self, new_switch):
+        # allow to modify self.switch variable outside of class
+        self.switch = new_switch
+
+    def switch_state(self):
+        return 0 if self.switch == 1 else 1
+
+    def state_command(self):
+        self.switch = self.switch_state()
+        return f"OUTP:GEN {self.switch}\n"
+
+    def _serial_handler(body):
+        # the sole purpose of this decorator is to prevent PSU timeouts
+        def wrapper(self, *arg, **kw):
+            self.serial_id.open()
+            time.sleep(.015)
+            body(self, *arg, **kw)
+            time.sleep(.015)
+            self.serial_id.close()
+
+        return wrapper
+
+    @_serial_handler
+    def set_state(self):
+        self.serial_id.write(self.state_command().encode())
+
+
+class Channel(Device):
+    def __init__(self, channel_id, serial_id):
+        super().__init__(serial_id)
+        self.channel_id = channel_id
+
+    @Device._serial_handler
+    def set_params(self, voltage, current):
+        self.serial_id.write(f"INST:NSEL {self.channel_id}\nAPPL {voltage}, {current}\n".encode())
+
+    def state_command(self):
+        self.switch = self.switch_state()
+        return f"INST:NSEL {self.channel_id}\nOUTP:SEL {self.switch}\n"
---
+++ automation_server/operators/request_handler.py
@@ -0,0 +1,56 @@
+def pass_data(data):
+    # this function creates variables that are passed to .html form
+    states = {
+        "ch1_state": data["states"]["ch1"],
+        "ch2_state": data["states"]["ch2"],
+        "ch3_state": data["states"]["ch3"],
+        "ch4_state": data["states"]["ch4"],
+        "out_state": data["states"]["general"],
+        "CH1V": data["params"]["ch1"]["voltage"],
+        "CH1A": data["params"]["ch1"]["current"],
+        "CH2V": data["params"]["ch2"]["voltage"],
+        "CH2A": data["params"]["ch2"]["current"],
+        "CH3V": data["params"]["ch3"]["voltage"],
+        "CH3A": data["params"]["ch3"]["current"],
+        "CH4V": data["params"]["ch4"]["voltage"],
+        "CH4A": data["params"]["ch4"]["current"]}
+    return states
+
+
+class RequestHandler:
+    # this class is for converting and transferring data from requests
+
+    def __init__(self, channels):
+        self.channels = list(channels.values())
+
+    def state_setter(self, requested, memory):
+        if 'ch1' in requested:
+            self.channels[0].set_state()
+            memory.update_states("ch1", self.channels[0].switch)
+        if 'ch2' in requested:
+            self.channels[1].set_state()
+            memory.update_states("ch2", self.channels[1].switch)
+        if 'ch3' in requested:
+            self.channels[2].set_state()
+            memory.update_states("ch3", self.channels[2].switch)
+        if 'ch4' in requested:
+            self.channels[3].set_state()
+            memory.update_states("ch4", self.channels[3].switch)
+        if 'main' in requested:
+            self.channels[4].set_state()
+            memory.update_states("general", self.channels[4].switch)
+
+    def params_setter(self, requested, memory):
+        params = requested.values()
+        if 'ch1v' in requested:
+            self.channels[0].set_params(*params)
+            memory.update_params("ch1", *params)
+        if 'ch2v' in requested:
+            self.channels[1].set_params(*params)
+            memory.update_params("ch2", *params)
+        if 'ch3v' in requested:
+            self.channels[2].set_params(*params)
+            memory.update_params("ch3", *params)
+        if 'ch4v' in requested:
+            self.channels[3].set_params(*params)
+            memory.update_params("ch4", *params)
